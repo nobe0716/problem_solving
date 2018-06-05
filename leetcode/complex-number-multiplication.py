e1b69e2546744b01886840d6b9adfb9527018d2d
sunghyo.jung <sunghyo.jung@navercorp.com>
2018-06-05 13:22:28 +0900

    add solved, complex-number-multiplication

---
+++ leetcode/complex-number-multiplication.py
@@ -0,0 +1,29 @@
+class ComplexNumber:
+	def __init__(self, d, i):
+		self.d = d
+		self.i = i
+
+	def of(s):
+		d = int(s.split('+')[0])
+		i = int(s.split('+')[1][:-1])
+		return ComplexNumber(d, i)
+
+	def pro(self, cn):
+		d = self.d * cn.d - self.i * cn.i
+		i = self.d * cn.i + self.i * cn.d
+		return ComplexNumber(d, i)
+
+	def __str__(self):
+		return str(self.d) + '+' + str(self.i) + 'i'
+
+
+class Solution:
+	def complexNumberMultiply(self, a, b):
+		"""
+		:type a: str
+		:type b: str
+		:rtype: str
+		"""
+		ca = ComplexNumber.of(a)
+		cb = ComplexNumber.of(b)
+		return str(ca.pro(cb))

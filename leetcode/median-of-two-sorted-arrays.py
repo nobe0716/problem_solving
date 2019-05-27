3059e40a083a22c2e4fb1fd8af4bd65194cf5130
nobe0716 <nobe0716@gmail.com>
2019-05-28 01:57:15 +0900

    Resolve median-of-two-sorted-arrays.py

--- leetcode/median-of-two-sorted-arrays.py
+++ leetcode/median-of-two-sorted-arrays.py
@@ -1,44 +1,81 @@
+"""
+## Name of Prob
+Median of Two Sorted Arrays
+
+## Link
+https://leetcode.com/problems/median-of-two-sorted-arrays/
+
+## Note
+Median; values which divides array two equal-sized sub-arrays
+    - odd size, median belong to given array
+    - even size, median is half of mid two values
+
+'Two' sorted arrays (individually sorted)
+
+- there are (len(A) + len(B)) // 2 number, which are smaller than median
+
+## Input
+num1, num2
+
+## Output
+median of two sorted array
+
+## Strategy
+let half = (len(A) + len(B) + 1) // 2,
+
+Suppose len(A) < len(B) for convenience, we can swap if len(A) > len(B)
+
+First find valid index 'i' of A and 'j' of B
+A[:i], B[:j] values will be smaller than median
+num1[i] >= num2[j - 1], num1[i - 1] <= num2[j]
+
+Use binary search to find i on A
+j is determined by i, i + j == (m + n + 1) // 2 => j = (m + n + 1) // 2 - i
+
+
+then infer median or two mid values from i and j
+
+"""
+
+
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2):
-        num1, num2 = nums1, nums2
-        if len(num1) > len(num2):
-            num1, num2 = num2, num1
-        m, n = len(num1), len(num2)
-        half_len = (n + m + 1) // 2
-
+        m, n = len(nums1), len(nums2)
+        if m > n:
+            m, n, nums1, nums2 = n, m, nums2, nums1
         lo, hi = 0, m
-
+        half = (m + n + 1) // 2
         while lo <= hi:
-            mid = (lo + hi) // 2
-            j = half_len - mid
+            i = (lo + hi) // 2
+            j = half - i
 
-            if mid < hi and num2[j - 1] > num1[mid]:
-                lo = mid + 1
-            elif mid > lo and num2[j] < num1[mid - 1]:
-                hi = mid - 1
+            if i < hi and nums1[i] < nums2[j - 1]:
+                lo = i + 1
+            elif i > lo and nums1[i - 1] > nums2[j]:
+                hi = i - 1
             else:
-                max_left = 0
-                if mid == 0:
-                    max_left = num2[j - 1]
+                left_highest = None
+                if i == 0:
+                    left_highest = nums2[j - 1]
                 elif j == 0:
-                    max_left = num1[mid - 1]
+                    left_highest = nums1[i - 1]
                 else:
-                    max_left = max(num1[mid - 1], num2[j - 1])
+                    left_highest = max(nums1[i - 1], nums2[j - 1])
 
                 if (m + n) % 2 == 1:
-                    return max_left * 1.0
+                    return left_highest * 1.0
 
-                min_right = 0
-                if mid == m:
-                    min_right = num2[j]
+                right_smallest = None
+                if i == m:
+                    right_smallest = nums2[j]
                 elif j == n:
-                    min_right = num1[mid]
+                    right_smallest = nums1[i]
                 else:
-                    min_right = min(num1[mid], num2[j])
-                # print(max_left, min_right)
-                return (max_left + min_right) / 2.0
-        return 0
+                    right_smallest = min(nums1[i], nums2[j])
+                # print(left_highest, right_smallest)
+                return (left_highest + right_smallest) / 2.0
 
 
 solution = Solution()
-print(solution.findMedianSortedArrays([1, 4], [2, 3]))
+assert solution.findMedianSortedArrays([1, 3], [2]) == 2.0
+assert solution.findMedianSortedArrays([1, 4], [2, 3]) == 2.5

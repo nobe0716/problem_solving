871e777a4a7f6d2a90741bd6de7d5e6f718463b3
sunghyo-jung <sunghyo.jung@navercorp.com>
2019-09-11 18:45:36 +0900

    Updated solved prob

--- codeforces/contests/1214/D. Treasure Island.py
+++ codeforces/contests/1214/D. Treasure Island.py
@@ -1,15 +1,40 @@
+from collections import defaultdict
+from sys import stdin
+
 n, m = map(int, input().split())
+
 g = ['.' * (m + 1)]
 for _ in range(n):
-    g.append('.' + input())
+    g.append('.' + stdin.readline())
 
-t = [[0 for i in range(m + 1)] for j in range(n + 1)]
-t[0][1] = 1
-for i in range(1, n + 1):
-    for j in range(1, m + 1):
-        if g[i][j] == '.':
-            t[i][j] = t[i - 1][j] + t[i][j - 1]
-print(t[n][m])
 
-for i in range(1, n + 1):
-    print(t[i][1:])
+def solve(n, m, g):
+    t = [[0] * (m + 2) for j in range(n + 2)]
+    t[0][1] = 1
+    t[n + 1][m] = 1
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if g[i][j] == '#':
+                t[i][j] = 0
+            else:
+                t[i][j] = max(t[i - 1][j], t[i][j - 1])
+    if t[n][m] == 0: # not rechable
+        return 0
+    d = defaultdict(int)
+    for i in range(n, 0, -1):
+        for j in range(m, 0, -1):
+            if t[i][j] == 0:
+                continue
+            else:
+                t[i][j] = max(t[i + 1][j], t[i][j + 1])
+
+            if t[i][j] == 1:
+                v = i - 1 + j - 1
+                d[v] += 1
+
+    return min(d[x] for x in range(1, n + m - 2))
+
+
+r = solve(n, m, g)
+print(r)

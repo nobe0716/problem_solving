8ec44bb96309d5ee5cebf54b0286eec011401be7
nobe0716 <nobe0716@gmail.com>
2020-01-16 22:07:29 +0900

    Do not take it sooooooooo hard :(
    https://leetcode.com/problems/minimum-distance-to-type-a-word-using-two-fingers/

--- leetcode/contests/weekly-contest-171/minimum-distance-to-type-a-word-using-two-fingers.py
+++ leetcode/contests/weekly-contest-171/minimum-distance-to-type-a-word-using-two-fingers.py
@@ -5,7 +5,6 @@ from string import ascii_uppercase
 
 class Solution:
     def minimumDistance(self, word: str) -> int:
-        MAX_VAL = 1000000
         COL_SIZE = 6
         word = ' ' + word
         l = len(word)
@@ -20,40 +19,30 @@ class Solution:
             return abs(g[sw][0] - g[dw][0]) + abs(g[sw][1] - g[dw][1])
 
         ct = [[0] * l for _ in range(l)]
-        act = [[0] * l for _ in range(l)]
         for i in range(l - 1):
             for j in range(i + 1, l):
                 cost_ij = cost(i, j)
                 ct[i][j] = ct[j][i] = cost_ij
-                act[i][j] = act[i][j - 1] + cost(j - 1, j)
 
+        @functools.lru_cache(None)
+        def dp(i, j, k):
+            if i == l - 1 or j == l - 1:
+                return 0
+            return min(
+                dp(k + 1, j, k + 1) + ct[i][k + 1],
+                dp(i, k + 1, k + 1) + ct[j][k + 1])
+
+        min_val = dp(1, 0, 1)
         if _DEBUG:
-            print(time.time() - st)
+            print('len({}), min_val: {}, elapsed: {}'.format(len(word) - 1, min_val, time.time() - st))
             st = time.time()
 
-        min_val = None
-
-        @functools.lru_cache(maxsize=300 ** 3 * 2)
-        def dp(i, j, finger):
-            if i == 1:
-                if finger == 0:
-                    return act[0][j]
-                else:
-                    return MAX_VAL
-            base = act[i][j]
-            return min((dp(k, i - 1, 1 - finger) + ct[k - 1][i] + base) for k in range(1, i))
-
-        min_val = min(
-            min(dp(i, l - 1, 0) for i in range(1, l)),
-            min(dp(i, l - 1, 1) for i in range(2, l))
-        )
-        if _DEBUG:
-            print(time.time() - st)
-            st = time.time()
         return min_val
 
 
 _DEBUG = False
+
+_DEBUG = True
 
 s = Solution()
 assert s.minimumDistance(word="CAKE") == 3

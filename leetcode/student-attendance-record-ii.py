409a1aea81a9f71d693706e6b776c733e41b006b
sunghyo-jung <sunghyo.jung@navercorp.com>
2020-09-20 15:08:14 +0900

    Update solution

--- leetcode/student-attendance-record-ii.py
+++ leetcode/student-attendance-record-ii.py
@@ -1,19 +1,39 @@
+_MODER = 10 ** 9 + 7
+
+
 class Solution:
     def checkRecord(self, n: int) -> int:
         """
-             00 01 02 10 11 12
-              0  1  2  3  4  5 
+        t[0]: 0-A, 0-continous-L
+        t[1]: 0-A, 1-continous-L
+        t[2]: 0-A, 2-continous-L
+        t[3]: 1-A, 0-continous-L
+        t[4]: 1-A, 1-continous-L
+        t[5]: 1-A, 2-continous-L
+
+        initial t = [1, 0, 0, 0, 0, 0]
+
+        new one can be one of `ALP`
+
+        new t[0] = t[0] + t[1] + t[2] (append 'P')
+        new t[1] = t[0] (append 'L')
+        new t[2] = t[1] (append 'L')
+        new t[3] = t[0] + t[1] + t[2] (append 'A') + t[3] + t[4] + t[5] (append 'P')
+        new t[4] = t[3] (append 'L')
+        new t[5] = t[4] (append 'L')
         """
-        now = [1, 0, 0, 0, 0, 0]
-        VAL_X = 10 ** 9 + 7
-        for i in range(1, n + 1):
-            new = [
-                    (now[0] + now[1] + now[2]) % VAL_X,
-                    now[0],
-                    now[1],
-                    sum(now) % VAL_X,
-                    now[3],
-                    now[4]
-                  ]
-            now = new
-        return sum(now) % VAL_X 
+        t = [1] + [0] * 5
+        for i in range(n):
+            t = [
+                sum(t[0:3]) % _MODER,
+                t[0],
+                t[1],
+                sum(t) % _MODER,
+                t[3],
+                t[4],
+            ]
+        return sum(t) % _MODER
+
+
+s = Solution()
+assert s.checkRecord(2) == 8

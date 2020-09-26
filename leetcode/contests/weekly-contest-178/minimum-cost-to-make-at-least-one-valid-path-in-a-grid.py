0f0d54d99bafe51f78764705598cffe1e18f3ee5
sunghyo-jung <sunghyo.jung@navercorp.com>
2020-09-27 00:43:29 +0900

    Polish solution
    enhance runtime(628ms->356ms) and memory(25.5MB->15.1MB)

--- leetcode/contests/weekly-contest-178/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
+++ leetcode/contests/weekly-contest-178/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
@@ -1,68 +1,50 @@
-import sys
 from typing import List
 
-sys.setrecursionlimit(1_000_000)
-di = [0, 0, 0, 1, -1]
-dj = [0, 1, -1, 0, 0]
-
-MAX_VAL = 1_000_000
-IS_DEBUG = False
+dx = [None, 0, 0, 1, -1]
+dy = [None, 1, -1, 0, 0]
 
 
 class Solution:
     def minCost(self, grid: List[List[int]]) -> int:
-        def is_valid_coord(x, y):
-            return (0 <= x < m and 0 <= y < n)
+        if not grid:
+            return 0
+        n, m = len(grid), len(grid[0])
+        group_info = [[0] * m for _ in range(n)]
 
-        def print_table():
-            if not IS_DEBUG:
-                return
-            for i in range(m):
-                for j in range(n):
-                    if table[i][j] == MAX_VAL:
-                        print('?', end=' ')
-                    else:
-                        print(table[i][j], end=' ')
-                print('')
+        def is_valid_coord(x: int, y: int):
+            return 0 <= x < n and 0 <= y < m
 
-        def fill_table(i, j, v):
-            if not is_valid_coord(i, j) or table[i][j] != MAX_VAL:
-                return []
-            table[i][j] = v
-            k = grid[i][j]
-            return [(i, j)] + fill_table(i + di[k], j + dj[k], v)
+        def expand_group(x: int, y: int, group: int):
+            stack = [(x, y)]
+            visited = set()
+            while stack:
+                x, y = stack.pop()
+                if not is_valid_coord(x, y) or group_info[x][y] > 0:
+                    continue
+                visited.add((x, y))
+                group_info[x][y] = group
+                d = grid[x][y]
+                stack.append((x + dx[d], y + dy[d]))
+            return visited
 
-        m, n = len(grid), len(grid[0])
-        table = [[MAX_VAL] * n for _ in range(m)]
-        # table[1][1] = 0
-        v = 0
-        pos_queue = fill_table(0, 0, v)
-
-        while pos_queue and table[m - 1][n - 1] == MAX_VAL:
-            print_table()
-            new_pos = []
-            v += 1
-            for i, j in pos_queue:
-                for k in range(1, 5):
-                    x, y = i + di[k], j + dj[k]
-                    if is_valid_coord(x, y) and table[x][y] == MAX_VAL:
-                        new_pos.append((x, y))
-
-            pos_queue = set()
-            for i, j in new_pos:
-                for x, y in fill_table(i, j, v):
-                    pos_queue.add((x, y))
-
-        print_table()
-
-        return v
+        group_count = 1
+        q = expand_group(0, 0, 1)
+        while q and group_info[n - 1][m - 1] == 0:
+            nq = set()
+            for x, y in q:
+                for i in range(1, 5):
+                    nx, ny = x + dx[i], y + dy[i]
+                    if not is_valid_coord(nx, ny) or group_info[nx][ny] > 0:
+                        continue
+                    nq |= expand_group(nx, ny, group_count + 1)
+            q = nq
+            group_count += 1
+        return group_info[n - 1][m - 1] - 1
 
 
-if __name__ == '__main__':
-    s = Solution()
-    IS_DEBUG = True
-    assert s.minCost(grid=[[1, 2], [4, 3]]) == 1
-    assert s.minCost(grid=[[2, 2, 2], [2, 2, 2]]) == 3
-    assert s.minCost(grid=[[4]]) == 0
-    assert s.minCost(grid=[[1, 1, 1, 1], [2, 2, 2, 2], [1, 1, 1, 1], [2, 2, 2, 2]]) == 3
-    assert s.minCost(grid=[[1, 1, 3], [3, 2, 2], [1, 1, 4]]) == 0
+s = Solution()
+assert s.minCost([[1, 1, 1, 1], [2, 2, 2, 2], [1, 1, 1, 1], [2, 2, 2, 2]]) == 3
+assert s.minCost(grid=[[1, 1, 3], [3, 2, 2], [1, 1, 4]]) == 0
+assert s.minCost(grid=[[1, 2], [4, 3]]) == 1
+assert s.minCost(grid=[[2, 2, 2], [2, 2, 2]]) == 3
+assert s.minCost(grid=[[4]]) == 0

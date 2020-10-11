28175774e3407afd6855ab56a4c2e2dbafe64c79
sunghyo-jung <sunghyo.jung@navercorp.com>
2020-10-11 13:22:07 +0900

    Polish solution

--- leetcode/contests/weekly-contest-171/minimum-distance-to-type-a-word-using-two-fingers.py
+++ leetcode/contests/weekly-contest-171/minimum-distance-to-type-a-word-using-two-fingers.py
@@ -1,43 +1,31 @@
-import functools
-import time
+from functools import lru_cache
 from string import ascii_uppercase
 
 
 class Solution:
     def minimumDistance(self, word: str) -> int:
-        COL_SIZE = 6
+        grid = {k: (i // 6, i % 6) for i, k in enumerate(ascii_uppercase)}
+
+        def dist(s, d):
+            return abs(grid[s][0] - grid[d][0]) + abs(grid[s][1] - grid[d][1])
+
         word = ' ' + word
-        l = len(word)
-        g = {ch: (i // COL_SIZE, i % COL_SIZE) for i, ch in enumerate(ascii_uppercase)}
-        st = time.time()
+        n = len(word)
+        ct = [[0] * n for _ in range(n)]
+        for i in range(1, n - 1):
+            for j in range(i + 1, n):
+                ct[j][i] = ct[i][j] = dist(word[i], word[j])
 
-        def cost(src, dst):
-            if src == 0 or dst == 0:
-                return 0
-            sw = word[src]
-            dw = word[dst]
-            return abs(g[sw][0] - g[dw][0]) + abs(g[sw][1] - g[dw][1])
-
-        ct = [[0] * l for _ in range(l)]
-        for i in range(l - 1):
-            for j in range(i + 1, l):
-                cost_ij = cost(i, j)
-                ct[i][j] = ct[j][i] = cost_ij
-
-        @functools.lru_cache(None)
-        def dp(i, j, k):
-            if i == l - 1 or j == l - 1:
+        @lru_cache(None)
+        def dp(i: int, j: int, k: int) -> int:
+            if k == n - 1:
                 return 0
             return min(
                 dp(k + 1, j, k + 1) + ct[i][k + 1],
-                dp(i, k + 1, k + 1) + ct[j][k + 1])
+                dp(i, k + 1, k + 1) + ct[j][k + 1]
+            )
 
-        min_val = dp(1, 0, 1)
-        if _DEBUG:
-            print('len({}), min_val: {}, elapsed: {}'.format(len(word) - 1, min_val, time.time() - st))
-            st = time.time()
-
-        return min_val
+        return dp(0, 1, 1)
 
 
 _DEBUG = False

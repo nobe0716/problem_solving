b54fbf06367e7c84ead1f0a2402862c75ec80101
sunghyo-jung <sunghyo.jung@navercorp.com>
2020-10-15 21:22:13 +0900

    resolved prob

--- leetcode/swap-adjacent-in-lr-string.py
+++ leetcode/swap-adjacent-in-lr-string.py
@@ -1,19 +1,24 @@
 class Solution(object):
     def canTransform(self, start, end):
-        s = set(['XL', 'RX'])
         start, end = list(start), list(end)
-        l = len(start)
-        for i in range(l):
-            while start[i] != end[i]:
-                j = i + 1
-                while j < l  and start[j] != end[i]:
-                    j += 1
-                if j >= l:
+        trans = {'XL', 'RX'}
+        stop = len(start)
+        for i in range(stop):
+            if start[i] == end[i]:
+                continue
+            j = i + 1
+            while j < stop and start[j] != end[i]:
+                j += 1
+            if j == stop:
+                return False
+            while j > i:
+                if (start[j - 1] + start[j]) in trans:
+                    start[j - 1], start[j] = start[j], start[j - 1]
+                    j -= 1
+                else:
                     return False
-                while j > i:
-                    if "".join(start[j-1:j+1]) in s:
-                        start[j - 1], start[j] = start[j], start[j - 1]
-                        j -= 1
-                    else:
-                        return False
         return True
+
+
+s = Solution()
+assert s.canTransform("RXXLRXRXL", "XRLXXRRLX")

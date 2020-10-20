612ea920512b0653867035f3004260be2a706d1a
sunghyo-jung <sunghyo.jung@navercorp.com>
2020-10-20 22:03:16 +0900

    Add solved probs

--- leetcode/guess-number-higher-or-lower-ii.py
+++ leetcode/guess-number-higher-or-lower-ii.py
@@ -1,35 +1,25 @@
-class Solution(object):
-    def __init__(self):
-        self.t = []
+import functools
 
-    def dp(self, a, b):
-        if self.t[a][b] is not None:
-            return self.t[a][b]
-        min_cost = 999999999999999999
-        for i in range(a + 1, b):
-            cost = i + max([self.dp(a, i - 1) , self.dp(i + 1, b)])
-            min_cost = min([cost, min_cost])
-        self.t[a][b] = min_cost
-        return min_cost
 
-    def getMoneyAmount(self, n):
-        self.t = [[None for x in range(n + 1)] for y in range(n + 1)]
-        for i in range(n + 1):
-            self.t[i][i] = 0
-            if i > 0:
-                self.t[i - 1][i] = i - 1
-            if i > 1:
-                self.t[i - 2][i] = i - 1
-        return self.dp(1, n)
+class Solution:
+    def getMoneyAmount(self, n: int) -> int:
+        @functools.lru_cache(None)
+        def dp(lo: int, hi: int) -> int:
+            if lo == hi:
+                return 0
+            if lo + 1 == hi:
+                return lo
+            elif lo > hi:
+                return 0
+            return min(i + max(dp(lo, i - 1), dp(i + 1, hi)) for i in range(lo, hi))
 
-l = []
-l2 = []
-l.append(l2)
+        return dp(1, n)
+
+
 s = Solution()
-print(s.getMoneyAmount(7))
-
-assert(s.getMoneyAmount(1) == 0)
-assert(s.getMoneyAmount(2) == 1)
-assert(s.getMoneyAmount(3) == 2)
-assert(s.getMoneyAmount(4) == 4)
-assert(s.getMoneyAmount(7) == 10)
+assert (s.getMoneyAmount(1) == 0)
+assert (s.getMoneyAmount(2) == 1)
+assert (s.getMoneyAmount(3) == 2)
+assert (s.getMoneyAmount(4) == 4)
+assert (s.getMoneyAmount(7) == 10)
+assert (s.getMoneyAmount(10) == 16)

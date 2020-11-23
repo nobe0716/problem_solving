62b339aadf10cf74769b6c08daff41fe4ba2333c
sunghyo-jung <sunghyo.jung@navercorp.com>
2020-11-23 23:30:35 +0900

    Polish solution

--- leetcode/longest-string-chain.py
+++ leetcode/longest-string-chain.py
@@ -1,49 +1,20 @@
-from collections import defaultdict
+import functools
 from typing import List
-
-
-def is_predecessor(w1, w2):
-    l = len(w1)
-    if l + 1 != len(w2):
-        return False
-    i = j = 0
-    while i < l and w1[i] == w2[j]:
-        i += 1
-        j += 1
-    j += 1
-    while i < l and w1[i] == w2[j]:
-        i += 1
-        j += 1
-    return i == l and j == l + 1
 
 
 class Solution:
     def longestStrChain(self, words: List[str]) -> int:
-        g = defaultdict(set)
+        @functools.lru_cache(None)
+        def dp(s: str) -> int:
+            v = [1]
+            for i in range(len(s)):
+                t = s[0:i] + s[i + 1:]
+                if t in word_set:
+                    v.append(dp(t) + 1)
+            return max(v)
 
-        l = len(words)
-        q = set(range(l))
-        nq = set()
-        for i in range(l):
-            for j in range(l):
-                if i == j:
-                    continue
-                if is_predecessor(words[i], words[j]):
-                    g[i].add(j)
-                    q.discard(j)
-
-        maximum_seq = defaultdict(int)
-        d = 1
-        while q:
-            nq = set()
-            for i in q:
-                maximum_seq[i] = d
-                for n in g[i]:
-                    nq.add(n)
-
-            d += 1
-            q = nq
-        return max(maximum_seq.values())
+        word_set = set(words)
+        return max(map(dp, words))
 
 
 s = Solution()

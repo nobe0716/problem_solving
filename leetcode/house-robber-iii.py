79d34a9cfc33ba8448ef02ccf78bac5e0f809a4c
sunghyo-jung <sunghyo.jung@navercorp.com>
2020-11-29 00:23:51 +0900

    Add resolved prob

--- leetcode/house-robber-iii.py
+++ leetcode/house-robber-iii.py
@@ -1,11 +1,17 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
-    def rob(self, root):
-        def search(r):
-            if r is None:
-                return [0, 0]
-            lv = search(r.left)
-            rv = search(r.right)
+    def rob(self, root: TreeNode) -> int:
+        def proc(node: TreeNode):
+            if not node:
+                return (0, 0)
+            lv, rv = map(proc, (node.left, node.right))
+            return (node.val + lv[1] + rv[1], max(lv) + max(rv))
 
-            return [r.val + lv[1] + rv[1], max(lv) + max(rv)]
-
-        return max(search(root))
+        return proc(root)

aa28bcfd1b48296860d60cfe7cc29f88d1f6be3e
sunghyo-jung <sunghyo.jung@navercorp.com>
2020-12-03 00:16:41 +0900

    Add new file to create a new ps file

---
+++ leetcode/create_new_file.py
@@ -0,0 +1,18 @@
+print('# enter url of prob')
+url_of_prob = input()
+url_of_prob = url_of_prob.strip()
+name_of_prob = url_of_prob.split('/')[-2]
+print('# file name will be `{}.py`'.format(name_of_prob))
+print('# past contents her. trl-D or Ctrl-Z ( windows ) to save it.')
+contents = []
+while True:
+    try:
+        line = input()
+    except EOFError:
+        break
+    contents.append(line)
+
+f = open(name_of_prob + '.py', 'w')
+f.write('\n'.join(contents))
+f.close()
+print('# write finished')

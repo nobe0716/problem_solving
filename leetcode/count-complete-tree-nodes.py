4bf81ad380ae1f7e340297d0af83799a91589af3
sunghyo-jung <sunghyo.jung@navercorp.com>
2021-02-27 21:35:14 +0900

    add solved

--- leetcode/count-complete-tree-nodes.py
+++ leetcode/count-complete-tree-nodes.py
@@ -1,35 +1,29 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-class Solution(object):
+from typing import Callable
 
-    def findLast(self, root, height, idx):
-        if height == 0:
-            return idx
-        left_idx = right_idx = 0
-        if root.right is not None:
-            right_idx = self.findLast(self, root.right, height - 1, idx * 2 + 1)
-        if root.left is not None:
-            left_idx = self.findLast(self, root.left, height - 1, idx * 2)
-        return max([left_idx, right_idx])
 
-    def countNodes(self, root):
-        if root is None:
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def countNodes(self, root: TreeNode) -> int:
+        if not root:
             return 0
-        if root.left is None and root.right is None:
-            return 1
 
-        # calc height
-        node = root
-        h = 0
-        while node is not None:
-            h += 1
-            node = node.left
+        def get_height(node: TreeNode, func: Callable[[TreeNode], TreeNode]) -> int:
+            height = 0
+            while node:
+                height += 1
+                node = func(node)
+            return height
 
-        # find last node
-        return self.findLast(root, h, 1)
+        lh = get_height(root, lambda node: node.left)
+        rh = get_height(root, lambda node: node.right)
 
-
+        if lh == rh:
+            return 2 ** lh - 1
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
